478745d341c685fcf53b506bcf5825ae9fae01b8
Leonardo Barros <leonardo.alves.barros.1@gmail.com>
2025-05-18 19:44:46 -0300

    WIP Level1_Farm

---
+++ IFRJam 9 - Tomate de Ritmo/Assets/Scripts/Rhythm System/Animation/PlayerAnimation.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] Transform[] animationTarget;
+    [SerializeField] Transform[] animationObjects;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
